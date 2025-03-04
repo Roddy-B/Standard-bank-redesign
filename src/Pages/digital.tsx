66294f707a33c45ba468907183b1ba0dfa4e74a5
Rodsone Fernandes <rodsonfernandes03@gmail.com>
2025-03-04 19:28:14 +0200

    digital 2.1

--- src/Pages/digital.tsx
+++ src/Pages/digital.tsx
@@ -6,18 +6,18 @@ import { Card } from '@/components/cards/page';
 export default function Digital() {
 
     return (
-        <div className=" mx-auto flex items-center justify-center p-28">
-            <div className="w-[1216px] flex  gap-16 justify-center bg-primary p-16 rounded ">
+        <div className=" mx-auto flex items-center justify-center p-28 font-franklin h-[720px]">
+            <div className=" flex  gap-16 justify-center bg-primary p-16 rounded">
                 <div className="flex flex-col items-start justify-center ">
                     <h1 className="text-4xl md:text-5xl font-medium font-franklin leading-lnH pt-4">
                         Digitalize sua vida
                     </h1>
-                    <p className="text-base md:text-lg font-base font-inter leading-lnL mt-4">
+                    <p className="text-base md:text-lg font-base  leading-lnL mt-4">
                         Conecte-se à modernidade com nossas soluções <br />
                         digitais - simplicidade e eficiência na ponta dos seus <br />
                         dedos.
                     </p>
-                    <div className="text-base font-semibold font-inter leading-lnL pt-4">
+                    <div className="text-base font-semibold leading-lnL pt-4">
                         <Link href="#" className="flex items-center gap-2" aria-label="Saber mais sobre digitalização">
                             Saber mais 
                             <IoIosArrowForward /> 
