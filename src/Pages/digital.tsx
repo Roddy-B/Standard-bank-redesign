fb56a7dac89227b86c99db327365f5bead2fe067
Rodsone Fernandes <rodsonfernandes03@gmail.com>
2025-03-04 17:25:03 +0200

    digital fix

--- src/Pages/digital.tsx
+++ src/Pages/digital.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 export default function Digital() {
     return (
-        <div className="flex justify-center items-center py-28">
-            <div className="flex w-full max-w-[1216px] h-auto md:h-[496px] mx-auto bg-primary py-14 px-4 md:px-28 justify-between rounded-lg">
+        <div className="flex justify-center items-center p-28">
+            <div className="flex w-full max-w-[1216px] md:h-[496px] mx-auto bg-primary items-center  justify-center gap-28 rounded-lg">
                 <div className="text-white">
                     <h1 className="text-4xl md:text-5xl font-medium font-franklin leading-lnH pt-4">
                         Digitalize sua vida
@@ -22,7 +22,7 @@ export default function Digital() {
                         </Link>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-8 mt-8 md:mt-0">
+                <div className="grid md:grid-cols-2 grid-rows-2 gap-8 mt-8 md:mt-0">
                     <Card imageSrc="/Bol.png" text="Bol" alt="Bol Logo" />
                     <Card imageSrc="/NetPlus.png" text="Net Plus" alt="Net Plus Logo" />
                     <Card imageSrc="/QuiQ.png" text="QuiQ" alt="QuiQ Logo" />
