6343a2b53a12273d2cde34e46c08581522e7fa14
Rodsone Fernandes <rodsonfernandes03@gmail.com>
2025-03-04 19:19:17 +0200

    digital 2.0

--- src/Pages/digital.tsx
+++ src/Pages/digital.tsx
@@ -1,12 +1,14 @@
-import { IoIosArrowForward } from "react-icons/io";
-import { Card } from './../components/cards/page';
 import Link from 'next/link';
+import { IoIosArrowForward } from "react-icons/io";
+import { Card } from '@/components/cards/page';
+
 
 export default function Digital() {
+
     return (
-        <div className="flex justify-center items-center p-28">
-            <div className="flex w-full max-w-[1216px] md:h-[496px] mx-auto bg-primary items-center  justify-center gap-28 rounded-lg">
-                <div className="text-white">
+        <div className=" mx-auto flex items-center justify-center p-28">
+            <div className="w-[1216px] flex  gap-16 justify-center bg-primary p-16 rounded ">
+                <div className="flex flex-col items-start justify-center ">
                     <h1 className="text-4xl md:text-5xl font-medium font-franklin leading-lnH pt-4">
                         Digitalize sua vida
                     </h1>
@@ -15,20 +17,26 @@ export default function Digital() {
                         digitais - simplicidade e eficiência na ponta dos seus <br />
                         dedos.
                     </p>
-                    <div className="text-base font-normal font-inter leading-lnL pt-4">
+                    <div className="text-base font-semibold font-inter leading-lnL pt-4">
                         <Link href="#" className="flex items-center gap-2" aria-label="Saber mais sobre digitalização">
                             Saber mais 
                             <IoIosArrowForward /> 
                         </Link>
                     </div>
                 </div>
-                <div className="grid md:grid-cols-2 grid-rows-2 gap-8 mt-8 md:mt-0">
-                    <Card imageSrc="/Bol.png" text="Bol" alt="Bol Logo" />
-                    <Card imageSrc="/NetPlus.png" text="Net Plus" alt="Net Plus Logo" />
-                    <Card imageSrc="/QuiQ.png" text="QuiQ" alt="QuiQ Logo" />
-                    <Card imageSrc="/ATM.png" text="Grande Depositos" alt="Grande Depositos Logo" />
+                <div>
+                    <div className=" flex flex-col gap-8 justify-between">
+                        <div className='flex gap-8'>
+                            <Card imageSrc="/Bol.png" text="Bol" alt="Bol Logo" />
+                            <Card imageSrc="/NetPlus.png" text="Net Plus" alt="Net Plus Logo" />
+                        </div>
+                        <div className='flex gap-8'>
+                            <Card imageSrc="/QuiQ.png" text="QuiQ" alt="QuiQ Logo" />
+                            <Card imageSrc="/ATM.png" text="Grande Depositos" alt="Grande Depositos Logo" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
