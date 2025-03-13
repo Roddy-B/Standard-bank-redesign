f042b551cd4c753bbc77ca10789aabbdedbb0471
Rodsone Fernandes <rodsonfernandes03@gmail.com>
2025-03-13 13:55:13 +0200

    images

--- src/Pages/service.tsx
+++ src/Pages/service.tsx
@@ -6,7 +6,7 @@ import { PiSuitcaseRolling } from "react-icons/pi";
 export default function Servicos() {
     return (
         <section className=" bg-primaryb flex justify-center items-center p-28">
-            <div className="w-[1216px] mx-auto">
+            <div className="w-[1216px] h-[596px] mx-auto">
                 <div className="mb-8 flex flex-col justify-center items-center text-wrap py-4">
                     <p className="text-5xl font-medium mb-4 font-inter text-bluetext leading-6">
                         Serviços bancários adaptados às
